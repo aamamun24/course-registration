212c93a0aa8e7d7dd68bdcd01cf22e62ba0b479a
aamamun24 <bdmamaun100@gmail.com>
2023-09-14 22:00:03 +0600

    project file created

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1 className='text-4xl text-center font-bold'>Course Registration</h1>
+    </>
+  )
+}
+
+export default App
