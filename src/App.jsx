176116272bf9aec71423411ea5a2967e5ada619a
aamamun24 <bdmamaun100@gmail.com>
2023-09-16 00:56:52 +0600

    remainig credit, total credit and price completed

--- src/App.jsx
+++ src/App.jsx
@@ -5,10 +5,31 @@ import Cards from './components/Cards/Cards'
 import Carts from './components/Carts/Carts'
 
 function App() {
-  const [carts, setCarts] = useState([])
-  const handleSelectToCart = (item) => {
-    setCarts([...carts, item])
+  const [carts, setCarts] = useState([]);
+  const [remaining, setRemaining] = useState(20)
+  const [credits, setCredits] = useState(0);
+  const [price, setPrice] = useState(0);
+
+  const handleSelectToCart = (card) => {
+    const already = carts.find(item => item.course_name === card.course_name);
+
+    if (already) {
+      return alert('already added')
+    }
+    else {
+      if (remaining <= 0) {
+        console.log(remaining);
+        return alert('You have not enough credits')
+      }
+      else {
+        setCarts([...carts, card]);
+        setCredits(credits + card.credit);
+        setRemaining(remaining - card.credit)
+        setPrice(price + card.price)
+      }
+    }
   }
+
   return (
     <>
       <Header></Header>
@@ -18,6 +39,9 @@ function App() {
         ></Cards>
         <Carts
           carts={carts}
+          credits={credits}
+          price={price}
+          remaining={remaining}
         ></Carts>
       </div>
     </>
--- src/components/Carts/Carts.jsx
+++ src/components/Carts/Carts.jsx
@@ -1,11 +1,11 @@
 import PropTypes from 'prop-types';
 import Cart from '../Cart/Cart';
 
-const Carts = ({ carts }) => {
+const Carts = ({ carts, credits, price, remaining }) => {
     return (
         <div className='md:w-1/3 lg:w-1/4'>
             <div className='bg-white p-6 rounded-lg'>
-                <h2 className='text-lg font-bold text-[#2F80ED] pb-4 border-b'>Credit Hour Remaining 7 hr</h2>
+                <h2 className='text-lg font-bold text-[#2F80ED] pb-4 border-b'>Credit Hour Remaining {remaining} hr</h2>
                 <div>
                     <h3 className='text-xl font-bold text-[#1C1B1B] mt-4'>Course Name</h3>
                     <ol className='my-5 list-decimal list-inside text-[#1C1B1B99] leading-7'>
@@ -17,15 +17,18 @@ const Carts = ({ carts }) => {
                         }
                     </ol>
                 </div>
-                <p className='text-[#1C1B1BCC] font-medium border-y py-4'>Total Credit Hour : 13</p>
-                <p className='text-[#1C1B1BCC] font-semibold mt-4'>Total Price : 48000 USD</p>
+                <p className='text-[#1C1B1BCC] font-medium border-y py-4'>Total Credit Hour : {credits}</p>
+                <p className='text-[#1C1B1BCC] font-semibold mt-4'>Total Price : {price} USD</p>
             </div>
         </div>
     );
 };
 
 Carts.propTypes = {
-    carts: PropTypes.array
+    carts: PropTypes.array,
+    credits: PropTypes.number,
+    price: PropTypes.number,
+    remaining: PropTypes.number
 };
 
 export default Carts;
